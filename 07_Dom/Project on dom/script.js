a198d3cced1b5983380933c684430250672398ad
Pranav Kumar <pranavkumar1522@gmail.com>
2024-03-15 21:42:56 +0530

    completed dom manipulation

---
+++ 07_Dom/Project on dom/script.js
@@ -0,0 +1,13 @@
+const color1 = document.querySelector('.color1');
+const color2 = document.querySelector('.color2');
+const css = document.querySelector('h3')
+const body = document.getElementById("body")
+
+
+function addGradient(){
+    body.style.background = "linear-gradient(to right," + color1.value + "," + color2.value + ")";
+}
+
+
+color1.addEventListener("input", addGradient);
+color2.addEventListener("input", addGradient);
