c32840bd654c754786600887842eabf8dff46c74
Pranav Kumar <pranavkumar1522@gmail.com>
2024-04-05 20:48:17 +0530

    Started the full stack project

---
+++ 15_facerecognitionbrain/src/App.js
@@ -0,0 +1,14 @@
+import Navigation from './components/Navigation/Navigation';
+import './App.css';
+import Logo from './components/Logo/Logo';
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+      <Logo/>
+    </div>
+  );
+}
+
+export default App;
---
+++ 15_facerecognitionbrain/src/components/Navigation/Navigation.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './Navigation.css'
+
+const Navigation = () => {
+  return (
+    <nav className="navbar">
+      <span className="signout">Sign Out</span>
+    </nav>
+  );
+};
+
+export default Navigation;
