fd57d09ae8423c98ab1b0e8f530741d9b800ff36
Pranav Kumar <pranavkumar1522@gmail.com>
2024-05-18 14:00:58 +0530

    more components

--- 20_ReactEccomerce/src/Pages/ShopCategory.jsx
+++ 20_ReactEccomerce/src/Pages/ShopCategory.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import './css/ShopCategory.css';
+import dropdown_icon from '../components/Assets/dropdown_icon.png';
+import { ShopContext } from '../Context/ShopContext';
+import Item from '../components/Item/Item';
 
-const ShopCategory = () => {
+const ShopCategory = (props) => {
+  const { allProducts } = useContext(ShopContext); // Destructure allProducts from context
+  const { banner, category } = props;
+
   return (
-    <div>ShopCategory</div>
-  )
-}
+    <div className="shop-category">
+      <img className='shopcategory-banner' src={banner} alt="banner" />
+      <div className="shopcategory-indexSort">
+        <p>
+          <span>Showing 1-12</span> out of 36 products
+        </p>
+        <div className="shopcategory-sort">
+          Sort by <img src={dropdown_icon} alt="dropdown icon" />
+        </div>
+      </div>
+      <div className="shopcategory-product">
+        {allProducts && allProducts
+          .filter(item => category === item.category)
+          .map(item => (
+            <Item
+              key={item.id}
+              i={item.id}
+              name={item.name}
+              image={item.image}
+              new_price={item.new_price}
+              old_price={item.old_price}
+            />
+          ))
+        }
+      </div>
+      <div className="shopcategory-loadmore">
+        Explore More
+      </div>
+    </div>
+  );
+};
 
-export default ShopCategory
+export default ShopCategory;
---
+++ 20_ReactEccomerce/src/components/Offers/Offers.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import exclusive_image from '../Assets/exclusive_image.png';
+import './Offer.css'
+
+const Offers = () => {
+  return (
+    <div className="offers">
+        <div className="offers-left">
+            <h1>Exclusive</h1>
+            <h1>Offers For You</h1>
+            <p>ONLY ON BEST SELLERS PRODUCT</p>
+            <button>Check Now</button>
+        </div>
+        <div className="offers-right">
+            <img src={exclusive_image} alt="img" />
+
+        </div>
+    </div>
+  )
+}
+
+export default Offers
